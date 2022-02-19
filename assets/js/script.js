2ff7f3ed5f10bf52887badfb48ec1147bb27852d
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-02-19 15:48:57 +0300

    style: work on signup and login forms, and their event handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 let card = document.querySelector(".card");
 let loginBtn = document.querySelector("#login");
+let signUpLink = document.querySelector("#sign-up-link");
+let loginLink = document.querySelector("#login-link");
 let loginForm = document.querySelector(".login-form");
+let signUpForm = document.querySelector(".signup-form");
 
 loginBtn.addEventListener("click", () => {
     card.style = "display: none;";
     loginForm.style = "display: flex;";
+});
+
+signUpLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    card.style = "display: none;";
+    loginForm.style = "display: none;";
+    signUpForm.style = "display: flex;";
+});
+
+loginLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    card.style = "display: none;";
+    loginForm.style = "display: flex;";
+    signUpForm.style = "display: none;";
 });
